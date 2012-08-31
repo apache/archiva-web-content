34f0ab759765d4ea86a802db122c69d66da723ed
Olivier Lamy <olamy@apache.org>
2012-08-31 10:15:03 +0000

    Apache Redback Components site deployment
    git-svn-id: http://svn.apache.org/repos/asf/archiva/site-content@1379401 13f79535-47bb-0310-9956-ffa450edef68

---
+++ redback/components/spring-taskqueue/apidocs/reference/lists.js
@@ -0,0 +1,16 @@
+var DATA = [
+      { id:0, label:"org.apache.archiva.redback.components.taskqueue", link:"org/apache/archiva/redback/components/taskqueue/package-summary.html", type:"package" },
+      { id:1, label:"org.apache.archiva.redback.components.taskqueue.DefaultTaskQueue", link:"org/apache/archiva/redback/components/taskqueue/DefaultTaskQueue.html", type:"class" },
+      { id:2, label:"org.apache.archiva.redback.components.taskqueue.Task", link:"org/apache/archiva/redback/components/taskqueue/Task.html", type:"class" },
+      { id:3, label:"org.apache.archiva.redback.components.taskqueue.TaskEntryEvaluator", link:"org/apache/archiva/redback/components/taskqueue/TaskEntryEvaluator.html", type:"class" },
+      { id:4, label:"org.apache.archiva.redback.components.taskqueue.TaskExitEvaluator", link:"org/apache/archiva/redback/components/taskqueue/TaskExitEvaluator.html", type:"class" },
+      { id:5, label:"org.apache.archiva.redback.components.taskqueue.TaskQueue", link:"org/apache/archiva/redback/components/taskqueue/TaskQueue.html", type:"class" },
+      { id:6, label:"org.apache.archiva.redback.components.taskqueue.TaskQueueException", link:"org/apache/archiva/redback/components/taskqueue/TaskQueueException.html", type:"class" },
+      { id:7, label:"org.apache.archiva.redback.components.taskqueue.TaskViabilityEvaluator", link:"org/apache/archiva/redback/components/taskqueue/TaskViabilityEvaluator.html", type:"class" },
+      { id:8, label:"org.apache.archiva.redback.components.taskqueue.execution", link:"org/apache/archiva/redback/components/taskqueue/execution/package-summary.html", type:"package" },
+      { id:9, label:"org.apache.archiva.redback.components.taskqueue.execution.TaskExecutionException", link:"org/apache/archiva/redback/components/taskqueue/execution/TaskExecutionException.html", type:"class" },
+      { id:10, label:"org.apache.archiva.redback.components.taskqueue.execution.TaskExecutor", link:"org/apache/archiva/redback/components/taskqueue/execution/TaskExecutor.html", type:"class" },
+      { id:11, label:"org.apache.archiva.redback.components.taskqueue.execution.TaskQueueExecutor", link:"org/apache/archiva/redback/components/taskqueue/execution/TaskQueueExecutor.html", type:"class" },
+      { id:12, label:"org.apache.archiva.redback.components.taskqueue.execution.ThreadedTaskQueueExecutor", link:"org/apache/archiva/redback/components/taskqueue/execution/ThreadedTaskQueueExecutor.html", type:"class" }
+
+    ];
---
+++ redback/components/spring-utils/apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "org.apache.archiva.redback.components.springutils", "org/apache/archiva/redback/components/springutils/package-summary.html", [ [ "Classes", null, [ [ "CachingByTypeBeanFactory", "org/apache/archiva/redback/components/springutils/CachingByTypeBeanFactory.html", null, "" ], [ "CachingWebApplicationContext", "org/apache/archiva/redback/components/springutils/CachingWebApplicationContext.html", null, "" ], [ "ComponentContainer", "org/apache/archiva/redback/components/springutils/ComponentContainer.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.apache.archiva.redback.components.springutils.plexusshim", "org/apache/archiva/redback/components/springutils/plexusshim/package-summary.html", [ [ "Classes", null, [ [ "PlexusShimComponent", "org/apache/archiva/redback/components/springutils/plexusshim/PlexusShimComponent.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
