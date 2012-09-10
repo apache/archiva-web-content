25362a63530b57f9c347e4ba0a7f74d2f234fda3
Olivier Lamy <olamy@apache.org>
2012-09-10 09:02:44 +0000

    Apache Archiva Main site deployment
    git-svn-id: http://svn.apache.org/repos/asf/archiva/site-content@1382708 13f79535-47bb-0310-9956-ffa450edef68

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     $('#dialogPreview').modal('show');
   })
 
-  $("#carousel-main" ).addClass("bs-docs-example");
+  $("#carousel-main" ).addClass("features-preview");
 
 
 });
