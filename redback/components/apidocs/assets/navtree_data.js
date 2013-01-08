475c17f4cdc41d88f3b6ed75bb46d9fb552665d0
Olivier Lamy <olamy@apache.org>
2013-01-08 23:10:10 +0000

    Apache Redback Components site deployment
    git-svn-id: http://svn.apache.org/repos/asf/archiva/site-content@1430621 13f79535-47bb-0310-9956-ffa450edef68

--- redback/components/apidocs/assets/navtree_data.js
+++ redback/components/apidocs/assets/navtree_data.js
@@ -47,7 +47,7 @@ var NAVTREE_DATA =
 , "" ] ]
 , "" ], [ "org.apache.archiva.redback.components.scheduler.configuration", "org/apache/archiva/redback/components/scheduler/configuration/package-summary.html", [ [ "Classes", null, [ [ "SchedulerConfiguration", "org/apache/archiva/redback/components/scheduler/configuration/SchedulerConfiguration.html", null, "" ] ]
 , "" ] ]
-, "" ], [ "org.apache.archiva.redback.components.springutils", "org/apache/archiva/redback/components/springutils/package-summary.html", [ [ "Classes", null, [ [ "CachingByTypeBeanFactory", "org/apache/archiva/redback/components/springutils/CachingByTypeBeanFactory.html", null, "" ], [ "CachingWebApplicationContext", "org/apache/archiva/redback/components/springutils/CachingWebApplicationContext.html", null, "" ], [ "ComponentContainer", "org/apache/archiva/redback/components/springutils/ComponentContainer.html", null, "" ] ]
+, "" ], [ "org.apache.archiva.redback.components.springutils", "org/apache/archiva/redback/components/springutils/package-summary.html", [ [ "Classes", null, [ [ "ComponentContainer", "org/apache/archiva/redback/components/springutils/ComponentContainer.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "org.apache.archiva.redback.components.springutils.plexusshim", "org/apache/archiva/redback/components/springutils/plexusshim/package-summary.html", [ [ "Classes", null, [ [ "PlexusShimComponent", "org/apache/archiva/redback/components/springutils/plexusshim/PlexusShimComponent.html", null, "" ] ]
 , "" ] ]
--- redback/components/spring-utils/apidocs/reference/lists.js
+++
@@ -1,9 +0,0 @@
-var DATA = [
-      { id:0, label:"org.apache.archiva.redback.components.springutils", link:"org/apache/archiva/redback/components/springutils/package-summary.html", type:"package" },
-      { id:1, label:"org.apache.archiva.redback.components.springutils.CachingByTypeBeanFactory", link:"org/apache/archiva/redback/components/springutils/CachingByTypeBeanFactory.html", type:"class" },
-      { id:2, label:"org.apache.archiva.redback.components.springutils.CachingWebApplicationContext", link:"org/apache/archiva/redback/components/springutils/CachingWebApplicationContext.html", type:"class" },
-      { id:3, label:"org.apache.archiva.redback.components.springutils.ComponentContainer", link:"org/apache/archiva/redback/components/springutils/ComponentContainer.html", type:"class" },
-      { id:4, label:"org.apache.archiva.redback.components.springutils.plexusshim", link:"org/apache/archiva/redback/components/springutils/plexusshim/package-summary.html", type:"package" },
-      { id:5, label:"org.apache.archiva.redback.components.springutils.plexusshim.PlexusShimComponent", link:"org/apache/archiva/redback/components/springutils/plexusshim/PlexusShimComponent.html", type:"class" }
-
-    ];
